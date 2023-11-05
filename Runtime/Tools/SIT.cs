b488e6234fe5bf8e02708483545f9bb92cabb476
moonzy <twenty2hmz@qq.com>
2023-11-05 23:11:57 +0800

    - 自增工具类

---
+++ Runtime/Tools/SIT.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Twenty2.VomitLib.Tools
+{
+    /// <summary>
+    /// 自增工具
+    /// </summary>
+    public static class SIT
+    {
+        private static Dictionary<System.Type, int> _map = new();
+
+        public static int NextID<T>()
+        {
+            var key = typeof(T);
+            if (!_map.TryGetValue(key, out var result))
+            {
+                _map.Add(key, int.MinValue);
+            }
+
+            result = _map[key];
+            _map[key]++;
+            return result;
+        }
+    }
+}
