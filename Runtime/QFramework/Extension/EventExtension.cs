d21855d68256196080497f0418201b63654c59fb
moonzy <twenty2hmz@qq.com>
2023-11-07 16:52:06 +0800

    - 修复BUG

--- Runtime/QFramework/Extension/EventExtension.cs
+++ Runtime/QFramework/Extension/EventExtension.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Cysharp.Threading.Tasks;
+using QFramework;
 
 namespace Twenty2.VomitLib
 {
     // TODO List池.
+    // TODO 异步事件标签缓存
     public static class EventExtension
     {
         public static Dictionary<string, List<UniTask>> s_taskListMap = new();
+        
+        public static Dictionary<Type, List<UniTask>> s_asyncEventCache = new();
 
         /// <summary>
         /// 返回当前事件的任务列表, 如果没有则新建一个.
@@ -31,28 +37,33 @@ namespace Twenty2.VomitLib
         /// <param name="task"></param>
         public static void AddTask<T>(this T e, UniTask task) where T : struct
         {
-            GetTaskList(e).Add(task);
+            var taskList = GetTaskList(e);
+            taskList.Add(task);
+            
+            LogKit.I($"{e.GetType().Name} 添加任务!! 当前任务数量 : {taskList.Count}");
         }
 
         /// <summary>
         /// 返回所有任务的 WhenAll 事件.
         /// 返回后清空事件.
         /// </summary>
-        public static async UniTask WhenAll<T>(this T e) where T : struct
+        public static UniTask WhenAll<T>(this T e) where T : struct
         {
             string key = typeof(T).Name;
         
-            if (!s_taskListMap.ContainsKey(key)) return;
-        
-            await UniTask.WhenAll(GetTaskList(e));
-        
-            s_taskListMap.Remove(key);
+            if (!s_taskListMap.ContainsKey(key))
+            {
+                LogKit.I($"{key} 没有异步任务!");
+                return UniTask.CompletedTask;
+            }
+
+            return UniTask.Create(async () =>
+            {
+                await UniTask.WhenAll(GetTaskList(e));
+
+                s_taskListMap.Remove(key);
+            });
         }
-    
-        public static UniTask SendAsync<T>(this T e) where T : struct
-        {
-            Vomit.Interface.SendEvent(e);
-            return e.WhenAll();
-        }
+        
     }
 }
