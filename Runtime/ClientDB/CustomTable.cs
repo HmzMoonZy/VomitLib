a5653ce303b72bb6a03fa17bf3bd873aefd491b8
moonzy <twenty2hmz@qq.com>
2023-11-09 00:37:17 +0800

    - CustomTable<T>

---
+++ Runtime/ClientDB/CustomTable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Twenty2.VomitLib.ClientDB
+{
+    /// <summary>
+    /// 和 Luban cs-simple-json 生成代码风格一致的数据表
+    /// </summary>
+    public class CustomTable<T> where T : ICustomTableItem
+    {
+        private readonly List<T> _dataList;
+        private readonly Dictionary<int, T> _dataMap;
+
+        
+        public CustomTable(string json)
+        {
+            _dataList = JsonConvert.DeserializeObject<List<T>>(json);
+            _dataMap = new Dictionary<int, T>();
+
+            foreach (var data in _dataList)
+            {
+                _dataMap.Add(data.GetID(), data);
+            }
+        }
+
+        public T this[int id] => _dataMap[id];
+    }
+}
---
+++ Runtime/ClientDB/ICustomTableItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Twenty2.VomitLib.ClientDB
+{
+    public interface ICustomTableItem : IComparable
+    {
+        public int GetID();
+
+        public string GetName();
+    }
+}
