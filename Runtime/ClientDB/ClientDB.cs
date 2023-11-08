4cc1521ce9cf321d32a8dcdf7c9a7f5a4738f453
moonzy <twenty2hmz@qq.com>
2023-11-09 01:07:59 +0800

    - 移除不必要的代码

--- Runtime/ClientDB/ClientDB.cs
+++ Runtime/ClientDB/ClientDB.cs
@@ -27,73 +27,10 @@ namespace Twenty2.VomitLib.ClientDB
 
         public static bool IsValid => _isValid;
 
-
-        // public static JSONNode Loader(string fileName)
-        // {
-        //     string sourcePath = Path.Combine(Vomit.RuntimeConfig.ClientDBConfig.JsonOutputPath,
-        //         $"{fileName}.{Vomit.RuntimeConfig.ClientDBConfig.Suffix}");
-        //     Debug.Log(sourcePath);
-        //     return JSON.Parse(File.ReadAllText(sourcePath));
-        //
-        //     // if (Application.platform != RuntimePlatform.Android)
-        //     // {
-        //     //     string sourcePath = Path.Combine(Application.streamingAssetsPath, "DB", fileName + ".dat");
-        //     //     return JSON.Parse(File.ReadAllText(sourcePath));
-        //     // }
-        //
-        //     // var request = UnityWebRequest.Get(Path.Combine(Application.streamingAssetsPath, "DB", fileName + ".dat"));
-        //     // request.SendWebRequest();
-        //     // while (true)
-        //     // {
-        //     //     if (request.downloadHandler.isDone)
-        //     //     {
-        //     //         return JSON.Parse(request.downloadHandler.text);
-        //     //     }
-        //     // }
-        // }
-
         public static void Init(object t)
         {
             T = (TTable) t;
             _isValid = true;
         }
-
-        // public static void SwitchLanguage(Language language)
-        // {
-        //     if (!IsValid)
-        //     {
-        //         return;
-        //     }
-        //
-        //     switch (language)
-        //     {
-        //         case Language.ChineseSimplified:
-        //             T.TranslateText(TextMapperCN);
-        //             break;
-        //         case Language.English:
-        //             T.TranslateText(TextMapperEN);
-        //             break;
-        //         default:
-        //             T.TranslateText(TextMapperEN);
-        //             break;
-        //     }
-        //
-        //     LogKit.I($"ClientDB Language : {language}");
-        //
-        //     string TextMapperCN(string key, string originText)
-        //     {
-        //         return T.TbLocalization.GetOrDefault(key)?.TextCn ?? originText;
-        //     }
-        //
-        //     string TextMapperEN(string key, string originText)
-        //     {
-        //         return T.TbLocalization.GetOrDefault(key)?.TextEn ?? originText;
-        //     }
-        // }
-        //
-        // public static string Translate(string key)
-        // {
-        //     return T.TbLocalization.GetOrDefault(key)?.TextCn ?? key;
-        // }
     }
 }
