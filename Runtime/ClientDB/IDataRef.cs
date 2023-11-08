de156bbc30ba80b41bb29949cb47a3d5019bb6e9
moonzy <twenty2hmz@qq.com>
2023-11-09 01:06:42 +0800

    - 提供IDataRef接口

---
+++ Runtime/ClientDB/IDataRef.cs
@@ -0,0 +1,16 @@
+﻿namespace Twenty2.VomitLib.ClientDB
+{
+    public interface IDataRef<out IDType, out DataType>
+    {
+        /// <summary>
+        /// 数据ID
+        /// </summary>
+        public IDType DataID { get; }
+
+        /// <summary>
+        /// 只读的数据
+        /// </summary>
+        /// <returns></returns>
+        public DataType GetData();
+    }
+}
