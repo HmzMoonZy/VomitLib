09a023afbef7309eff7aa84e375e62276dc4e2b0
moonzy <twenty2hmz@qq.com>
2023-11-11 20:57:19 +0800

    - Procedure 支持自动取消注册事件

--- Runtime/Procedure/Procedure.cs
+++ Runtime/Procedure/Procedure.cs
@@ -7,13 +7,13 @@ namespace Twenty2.VomitLib.Procedure
 {
     public static class Procedure<T> where T : struct
     {
-        public static FSM<T> s_fsm;
+        public static FSM<T> Fsm;
 
         private static T s_entryID;
         
         public static void Init()
         {
-            s_fsm = new();
+            Fsm = new();
             
             foreach (var assembly in System.AppDomain.CurrentDomain.GetAssemblies())
             {
@@ -27,7 +27,7 @@ namespace Twenty2.VomitLib.Procedure
 
                     LogKit.I($"创建 Procedure {id}");
                     
-                    s_fsm.AddState(id, obj);
+                    Fsm.AddState(id, obj);
                     
                     if (attr.IsEntry) s_entryID = id;
                 }
@@ -35,27 +35,27 @@ namespace Twenty2.VomitLib.Procedure
 
             UniTask.WaitWhile(() =>
             {
-                s_fsm.Update();
+                Fsm.Update();
                 return true;
             }, PlayerLoopTiming.Update).Forget();
             
             UniTask.WaitWhile(() =>
             {
-                s_fsm.FixedUpdate();
+                Fsm.FixedUpdate();
                 return true;
             }, PlayerLoopTiming.FixedUpdate).Forget();
         }
 
         public static void Start()
         {
-            s_fsm.StartState(s_entryID);
+            Fsm.StartState(s_entryID);
         }
 
         public static void Change(T id)
         {
-            LogKit.I($"切换状态! {s_fsm.PreviousStateId} => {id}");
+            LogKit.I($"切换状态! {Fsm.PreviousStateId} => {id}");
             
-            s_fsm.ChangeState(id);
+            Fsm.ChangeState(id);
         }
     }
 
--- Runtime/Procedure/ProcedureState.cs
+++ Runtime/Procedure/ProcedureState.cs
@@ -1,45 +1,95 @@
 ﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using QFramework;
 
 namespace Twenty2.VomitLib.Procedure
 {
     public abstract class ProcedureState<T> : ICanGetModel, ICanGetUtility, ICanGetSystem, ICanRegisterEvent, ICanSendEvent, ICanSendCommand, IState where T : struct
     {
-        protected FSM<T> _procedure => Procedure<T>.s_fsm;
+        /// <summary>
+        /// 状态机对象
+        /// </summary>
+        protected FSM<T> _procedure => Procedure<T>.Fsm;
+        
+        /// <summary>
+        /// 事件注册列表
+        /// </summary>
+        private List<IUnRegister> _registers = new();
 
+        /// <summary>
+        /// 当前状态ID
+        /// </summary>
         protected T CurrentStateID => _procedure.CurrentStateId;
+        
+        /// <summary>
+        /// 上一个状态ID
+        /// </summary>
+        protected T PreviousStateID => _procedure.PreviousStateId;
 
-        public abstract void Enter();
+        /// <summary>
+        /// 进入状态回调
+        /// </summary>
+        protected abstract void OnEnter();
 
-        public abstract void Exit();
+        /// <summary>
+        /// 退出状态回调
+        /// </summary>
+        protected  abstract void OnExit();
 
+        /// <summary>
+        /// 状态机切换条件
+        /// </summary>
         public abstract bool Condition();
+        
+        public void Enter()
+        {
+            OnEnter();
+        }
+        
+        public void Exit()
+        {
+            foreach (var unRegister in _registers)
+            {
+                unRegister.UnRegister();
+            }
+            OnExit();
+        }
 
+        /// <summary>
+        /// 每帧调用的方法
+        /// </summary>
         public virtual void Update()
         {
         }
 
+        /// <summary>
+        /// 固定帧率调用
+        /// </summary>
         public virtual void FixedUpdate()
         {
         }
-
-        /// <summary>
-        /// 永远不要实现这个方法,因为没有用.
-        /// </summary>
+        
+        [Obsolete("暂时不支持 OnGUI")]
         public virtual void OnGUI()
         {
         }
 
+
+
+        /// <summary>
+        /// 切换状态
+        /// </summary>
         protected void ChangeState(T id)
         {
             _procedure.ChangeState(id);
         }
 
-        // TODO 
-        [Obsolete]
-        protected void RegisterProcedureEvent()
+
+        protected void RegisterProcedureEvent<T>(Action<T> action) where T : struct
         {
-            // TODO 
+            _registers.Add(this.RegisterEvent<T>(action));
+            
         }
         
 
