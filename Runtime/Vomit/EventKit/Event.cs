21dcd566e92e628ff6abc41374271d33d5598794
moonzy <twenty2hmz@qq.com>
2023-11-15 18:40:47 +0800

    - API : 提供主动BreakWaitEvent

--- Runtime/Vomit/EventKit/Event.cs
+++ Runtime/Vomit/EventKit/Event.cs
@@ -10,6 +10,7 @@ namespace Twenty2.VomitLib
     {
         private static Dictionary<Type, IUnRegister> s_waitEventToken = new();
         
+        // TODO : UniTask.WhenAny 支持
         public static UniTask WaitEvent<T>() where T : struct
         {
             var key = typeof(T);
@@ -17,13 +18,49 @@ namespace Twenty2.VomitLib
             {
                 s_waitEventToken.Add(key, Vomit.Interface.RegisterEvent<T>(e =>
                 {
-                    s_waitEventToken[key].UnRegister();
-                    s_waitEventToken.Remove(typeof(T));
+                    BreakWaitEvent<T>();
                 }));
-
             }
 
             return UniTask.WaitWhile(() => s_waitEventToken.ContainsKey(typeof(T)));
+        }
+
+        public static void BreakAll()
+        {
+            foreach (var (key, unRegister) in s_waitEventToken)
+            {
+                s_waitEventToken[key].UnRegister();
+                s_waitEventToken.Remove(key);
+            }
+        }
+
+        public static void BreakWaitEvent(Type type)
+        {
+            try
+            {
+                s_waitEventToken[type].UnRegister();
+                s_waitEventToken.Remove(type);
+            }
+            catch (Exception e)
+            {
+                LogKit.E($"BreakWaitEvent Error : {type.Name}");
+            }
+
+        }
+        
+        public static void BreakWaitEvent<T>()
+        {
+            try
+            {
+                var key = typeof(T);
+                s_waitEventToken[key].UnRegister();
+                s_waitEventToken.Remove(typeof(T));
+            }
+            catch (Exception e)
+            {
+                LogKit.E($"BreakWaitEvent Error : {typeof(T).Name}");
+            }
+
         }
     }
 }
