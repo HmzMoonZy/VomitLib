7da1511ea555a985a0c2406116f37e75525391b4
moonzy <twenty2hmz@qq.com>
2023-11-18 00:38:24 +0800

    - CancellationSourceExtension

---
+++ Runtime/Vomit/CancellationSourceExtension/CancellationSourceExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace Twenty2.VomitLib
+{
+    public static class CancellationSourceExtension
+    {
+        public static void CancelAndDispose(this CancellationTokenSource cts)
+        {
+            cts.Cancel();
+            cts.Dispose();
+        }
+    }
+}
