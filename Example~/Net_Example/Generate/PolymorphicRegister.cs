79aaaaeaac9145cc287a597ed6859bc0d2379fd5
Mango_MoonZy <hmz464767144@gmail.com>
2024-05-23 11:59:40 +0800

    - 网络演示场景

---
+++ Example~/Net_Example/Generate/PolymorphicRegister.cs
@@ -0,0 +1,25 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using PolymorphicMessagePack;
+
+namespace Geek.Server.Proto
+{
+    public partial class PolymorphicRegister
+    {
+
+        static bool serializerRegistered = false;
+        private static void Init()
+        {
+            if (!serializerRegistered)
+            {
+                // PolymorphicResolver.AddInnerResolver(ConfigDataResolver.Instance);
+                PolymorphicResolver.AddInnerResolver(GeneratedResolver.Instance);
+                PolymorphicTypeMapper.Register<Message>();
+                PolymorphicResolver.Instance.Init();
+                serializerRegistered = true;
+            }
+        }
+
+        public static void Load() { Init(); }
+    }
+}
