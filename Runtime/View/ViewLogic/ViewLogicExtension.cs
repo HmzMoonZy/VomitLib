01593294bfa64811006f54c2774b12919962ad60
Mango_MoonZy <hmz464767144@gmail.com>
2024-05-23 11:00:15 +0800

    - ViewLogic提供泛型支持

--- Runtime/View/ViewLogic/ViewLogicExtension.cs
+++ Runtime/View/ViewLogic/ViewLogicExtension.cs
@@ -4,12 +4,6 @@ namespace Twenty2.VomitLib.View
 {
     public static class ViewLogicExtension
     {
-        public static UniTask<TLogic> WithEvent<TLogic, TEvent>(this UniTask<TLogic> task, TEvent eventParam)
-            where TLogic : ViewLogic 
-            where TEvent : struct
-        {
-            Vomit.Interface.SendEvent(eventParam);
-            return task;
-        }
+        
     }
 }
