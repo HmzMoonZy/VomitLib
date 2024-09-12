678f2a1b21923159cf60f99494d3366fa7bd83ea
Mango_MoonZy <hmz464767144@gmail.com>
2024-09-12 12:33:16 +0800

    - 修改库结构 - Config 新增网络字段

--- Runtime/_Core/Config/NetConfig.cs
+++ Runtime/_Core/Config/NetConfig.cs
@@ -5,6 +5,14 @@ namespace Twenty2.VomitLib.Config
     [Serializable]
     public class NetConfig
     {
+        public string GameServerIp;
+
+        public string GameServerPort;
+        
+        
+        
+        
+        
         public string ServerPath = "Vomit-Server/";
         
         public string ServerDataPath;
