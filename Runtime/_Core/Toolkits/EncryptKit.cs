696e1299db6e18fc414dcc11887667ba4062821f
Mango_MoonZy <hmz464767144@gmail.com>
2024-09-12 14:11:18 +0800

    - SimpleEncryptKit

---
+++ Runtime/_Core/Toolkits/EncryptKit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Twenty2.VomitLib.Tools
+{
+    public static class EncryptKit
+    {
+        public static string SimpleEncrypt(string str)
+        {
+            str = Convert.ToBase64String(Encoding.UTF8.GetBytes(str));
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+        
+        public static string SimpleDecrypt(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            var bytes = Convert.FromBase64String(new string(array));
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
