7bbfb94d335d619572b5a963fe821d0f1ada7e56
HmzMoonZy <hmz464767144@gmail.com>
2025-07-16 17:54:51 +0800

    - 移除不稳定的工具

--- Editor/ToolBar/ToolBarMenu.cs
+++ Editor/ToolBar/ToolBarMenu.cs
@@ -3,7 +3,6 @@ using System.IO;
 using LubanSupport.Editor;
 using Twenty2.VomitLib.Config;
 using Twenty2.VomitLib.Editor.MessagePack;
-using Twenty2.VomitLib.Editor.Server;
 using UnityEditor;
 using UnityEngine;
 
@@ -53,31 +52,6 @@ namespace Twenty2.VomitLib.Editor
             }
             
             GUILayout.Space(10);
-            
-            // 服务器管理按钮
-            var isServerRunning = ServerManagerTool.IsServerRunning;
-            var serverButtonColor = isServerRunning ? Color.green : Color.red;
-            var originalColor = GUI.backgroundColor;
-            
-            GUI.backgroundColor = serverButtonColor;
-            var serverButtonText = isServerRunning ? "停止服务器" : "启动服务器";
-            if (GUILayout.Button(serverButtonText, GUILayout.Width(80)))
-            {
-                if (isServerRunning)
-                {
-                    ServerManagerTool.StopServer();
-                }
-                else
-                {
-                    ServerManagerTool.StartServer();
-                }
-            }
-            GUI.backgroundColor = originalColor;
-            
-            if (GUILayout.Button("服务器输出", GUILayout.Width(80)))
-            {
-                ServerManagerTool.ShowOutputWindow();
-            }
 
             if (GUILayout.Button("服务器文件夹", GUILayout.Width(80)))
             {
