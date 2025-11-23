f1d309f571557d7f44504f4a0fbaaf66f1b38447
HmzMoonZy <hmz464767144@gmail.com>
2025-11-23 21:10:53 +0800

    - 添加编辑器调试方法 EnableMonitor

--- Runtime/_Core/Vomit.cs
+++ Runtime/_Core/Vomit.cs
@@ -19,9 +19,7 @@ namespace Twenty2.VomitLib
         /// </summary>
         /// <param name="architecture">关联框架的接口</param>
         /// <param name="logger">日志管理器</param>
-        public static void Init(
-            IArchitecture architecture, 
-            ILogHelper logger = null)
+        public static void Init(IArchitecture architecture, ILogHelper logger = null)
         {
             Log.SetLogHelper(logger ?? new DefaultLogHelper());
 
@@ -70,6 +68,21 @@ namespace Twenty2.VomitLib
                 Log.Debug($"Auto Load {type.Name}");
                 registerSystem!.MakeGenericMethod(type).Invoke(Interface, new[] {Activator.CreateInstance(type)});
             }
+        }
+
+        /// <summary>
+        /// 启动框架监视器, 仅在编辑器下有效
+        /// </summary>
+        public static void EnableMonitor()
+        {
+#if UNITY_EDITOR
+            new GameObject("VomitMonitor", typeof(Twenty2.VomitLib.Monitor.ViewMonitor));
+            new GameObject("ModelMonitor", typeof(Twenty2.VomitLib.Monitor.ModelMonitor));
+            new GameObject("SystemMonitor", typeof(Twenty2.VomitLib.Monitor.SystemMonitor));
+            new GameObject("EventMonitor", typeof(Twenty2.VomitLib.Monitor.EventMonitor));
+            new GameObject("CommandMonitor", typeof(Twenty2.VomitLib.Monitor.CommandMonitor));
+            new GameObject("LubanMonitor", typeof(Twenty2.VomitLib.Monitor.LubanSupportMonitor));
+#endif
         }
     }
     
