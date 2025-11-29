d9ac1e0a723bb1422f5eda00b9f940f9e05444f3
HmzMoonZy <hmz464767144@gmail.com>
2025-11-29 14:18:15 +0800

    - CommandButton

---
+++ Runtime/View/Components/CommndButton/CommandButton.cs
@@ -0,0 +1,41 @@
+﻿using QFramework;
+using Twenty2.VomitLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandButton : Button, IAbstractController
+{
+    private ICommand _command;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        onClick.AddListener(OnClick);
+    }
+
+    protected override void OnDestroy()
+    {
+        onClick.RemoveListener(OnClick);
+        base.OnDestroy();
+    }
+
+    public void SetCommand(ICommand command)
+    {
+        _command = command;
+    }
+    
+    public void SetCommand<T>() where T : ICommand, new()
+    {
+        _command = new T();
+    }
+    
+
+    private void OnClick()
+    {
+        if (_command != null)
+        {
+            ((IAbstractController) this).GetArchitecture().SendCommand(_command);    
+        }
+    }
+    
+}
