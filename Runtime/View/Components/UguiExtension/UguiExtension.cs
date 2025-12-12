94250f90ddbb139a359f2c412b5c0bedd7a737b2
HmzMoonZy <hmz464767144@gmail.com>
2025-12-12 15:36:03 +0800

    - 新增TextureKit工具类，提供ToSprite扩展方法 - 为Image组件添加SetTexture扩展方法 - 为Image组件添加SetSprite扩展方法

--- Runtime/View/Components/UguiExtension/UguiExtension.cs
+++ Runtime/View/Components/UguiExtension/UguiExtension.cs
@@ -1,6 +1,9 @@
 ﻿using QFramework;
-using Twenty2.VomitLib;
+using Twenty2.VomitLib.Tools;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 namespace Twenty2.VomitLib.View
@@ -24,6 +27,61 @@ namespace Twenty2.VomitLib.View
                 Vomit.Interface.SendCommand(command);
             });
         }
+
+        #endregion
+
+        #region Image
+
+        public static void SetSprite(this Image img, Sprite sprite)
+        {
+            img.sprite = sprite;
+        }
+
+        public static void SetTexture(this Image img, Texture2D texture)
+        {
+            img.sprite = texture.ToSprite();
+        }
+        
+        // private static Dictionary<string, Texture2D> s_urlTexture = new Dictionary<string, Texture2D>();
+        // public static async UniTask SetUrlTextureAsync(this Image img, string url)
+        // {
+        //     if (img == null)
+        //     {
+        //         return;
+        //     }
+        //     
+        //     s_urlTexture.TryGetValue(url, out Texture2D texture);
+        //     if (texture != null)
+        //     {
+        //         img.sprite = texture.ToSprite();
+        //         return;
+        //     }
+        //
+        //     var uwr = new UnityWebRequest(url);
+        //     var handler = new DownloadHandlerTexture(true);
+        //     uwr.downloadHandler = handler;
+        //     var response = await uwr.SendWebRequest();
+        //     if (response.result != UnityWebRequest.Result.Success)
+        //     {
+        //         Log.Error($"SetUrlTextureAsync Error! {response.error}");
+        //         return;
+        //     }
+        //     
+        //     if (handler.texture == null)
+        //     {
+        //         Log.Error($"SetUrlTextureAsync texture is null! url: {url}");
+        //         return;
+        //     }
+        //     
+        //     s_urlTexture[url] = handler.texture;
+        //
+        //     if (img == null || img.gameObject == null)
+        //     {
+        //         return;
+        //     }
+        //     
+        //     img.SetSprite(handler.texture.ToSprite());
+        // }
 
         #endregion
     }
---
+++ Runtime/_Toolkits/TextureKit/TextureKit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Twenty2.VomitLib.Tools
+{
+    public static class TextureKit
+    {
+        public static Sprite ToSprite(this Texture2D texture)
+        {
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        }
+    }
+}
