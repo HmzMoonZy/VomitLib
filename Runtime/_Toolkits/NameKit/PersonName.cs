141e8ff96a69ea41fca3a8bad5aa1aeb4a092e89
HmzMoonZy <hmz464767144@gmail.com>
2025-12-15 00:42:52 +0800

    - NameKit 占位

---
+++ Runtime/_Toolkits/NameKit/PersonName.cs
@@ -0,0 +1,25 @@
+﻿namespace Twenty2.VomitLib.Tools
+{
+    // TODO 
+    public static class PersonName
+    {
+        public enum Sex
+        {
+            Unknown,
+            Male,
+            Female
+        }
+        
+        public enum Country
+        {
+            China,
+            USA,
+            Japan,
+        }
+
+        public static string GetName(Country country = Country.USA, string surname = null, Sex sex = Sex.Unknown)
+        {
+            return "Unknown Name";
+        }
+    }
+}
