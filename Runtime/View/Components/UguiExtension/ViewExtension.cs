1ecc0bb993e30866aa81d53d873347ba0195b0b7
HmzMoonZy <hmz464767144@gmail.com>
2026-01-28 19:01:59 +0800

    - 移动一些API的结构

---
+++ Runtime/View/Components/UguiExtension/ViewExtension.cs
@@ -0,0 +1,34 @@
+﻿using QFramework;
+using Twenty2.VomitLib.Tools;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Twenty2.VomitLib.View
+{
+    public static class ViewExtension
+    {
+        #region Button - Command 
+
+        public static void BindCommand<T>(this Button selfBtn) where T : ICommand, new()
+        {
+            selfBtn.onClick.AddListener(() =>
+            {
+                Vomit.Interface.SendCommand(new T());
+            });
+        }
+        
+        public static void BindCommand<T>(this Button selfBtn, T command) where T : ICommand, new()
+        {
+            selfBtn.onClick.AddListener(() =>
+            {
+                Vomit.Interface.SendCommand(command);
+            });
+        }
+
+        #endregion
+    }
+}
+
--- Runtime/_FluentAPI/Unity/6.UnityEngineUIGraphicExtension.cs
+++ Runtime/_FluentAPI/Unity/6.UnityEngineUIGraphicExtension.cs
@@ -6,6 +6,7 @@
  * https://gitee.com/liangxiegame/QFramework
  ****************************************************************************/
 
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace FluentAPI
@@ -13,19 +14,12 @@ namespace FluentAPI
 
     public static class UnityEngineUIGraphicExtension
     {
-
         public static T ColorAlpha<T>(this T selfGraphic, float alpha) where T : Graphic
         {
             var color = selfGraphic.color;
             color.a = alpha;
             selfGraphic.color = color;
             return selfGraphic;
-        }
-        
-        public static Image FillAmount(this Image selfImage, float fillAmount)
-        {
-            selfImage.fillAmount = fillAmount;
-            return selfImage;
         }
         
         /// <summary>
@@ -35,5 +29,65 @@ namespace FluentAPI
         {
             selfBtn.onClick.RemoveAllListeners();
         }
+        
+        #region Image & Texture
+
+        public static Sprite ToSprite(this Texture2D texture)
+        {
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        }
+        
+        public static void SetSprite(this Image img, Sprite sprite)
+        {
+            img.sprite = sprite;
+        }
+
+        public static void SetTexture(this Image img, Texture2D texture)
+        {
+            img.sprite = texture.ToSprite();
+        }
+        
+        // private static Dictionary<string, Texture2D> s_urlTexture = new Dictionary<string, Texture2D>();
+        // public static async UniTask SetUrlTextureAsync(this Image img, string url)
+        // {
+        //     if (img == null)
+        //     {
+        //         return;
+        //     }
+        //     
+        //     s_urlTexture.TryGetValue(url, out Texture2D texture);
+        //     if (texture != null)
+        //     {
+        //         img.sprite = texture.ToSprite();
+        //         return;
+        //     }
+        //
+        //     var uwr = new UnityWebRequest(url);
+        //     var handler = new DownloadHandlerTexture(true);
+        //     uwr.downloadHandler = handler;
+        //     var response = await uwr.SendWebRequest();
+        //     if (response.result != UnityWebRequest.Result.Success)
+        //     {
+        //         Log.Error($"SetUrlTextureAsync Error! {response.error}");
+        //         return;
+        //     }
+        //     
+        //     if (handler.texture == null)
+        //     {
+        //         Log.Error($"SetUrlTextureAsync texture is null! url: {url}");
+        //         return;
+        //     }
+        //     
+        //     s_urlTexture[url] = handler.texture;
+        //
+        //     if (img == null || img.gameObject == null)
+        //     {
+        //         return;
+        //     }
+        //     
+        //     img.SetSprite(handler.texture.ToSprite());
+        // }
+
+        #endregion
     }
 }
